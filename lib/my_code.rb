ebc4d535210bdc86f0c11aa3892affa8177b0f27
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-10 15:36:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,21 +1,22 @@
-def map_to_negativize(source_array)
-  source_array.map { |num| num * -1}
-end
+# def map_to_negativize(source_array)
+#   source_array.map { |num| num * -1}
+# end
 
-def map_to_no_change(source_array)
-  source_array.map { |num| num * 1}
-end
+# def map_to_no_change(source_array)
+#   source_array.map { |num| num * 1}
+# end
 
-def map_to_double(source_array)
-  source_array.map { |num| num * 2}
-end
+# def map_to_double(source_array)
+#   source_array.map { |num| num * 2}
+# end
 
-def map_to_square(source_array)
-  source_array.map { |num| num ** 2}
-end
+# def map_to_square(source_array)
+#   source_array.map { |num| num ** 2}
+# end
 
-def reduce_to_total(source_array, starting_point = 0)   source_array.reduce(starting_point) { |sum, num| sum + num } 
-end
+# def reduce_to_total(source_array, starting_point = 0)   
+#   source_array.reduce(starting_point) { |sum, num| sum + num } 
+# end
 
 def reduce_to_any_true(source_array)
   source_array = [1,2,3]
